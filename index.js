b839a43a1c44eb237d60103ffddb76d47216f47d
Alan Martins <37884191+Alanmc021@users.noreply.github.com>
2023-09-11 16:59:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -12,20 +12,77 @@ admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://teste-908b8-default-rtdb.firebaseio.com"
 });
+//Ler toda base de dados : Produtos
+
+
+
+//Criar um novo produto
+
+app.get("/produtos", (req, res) => {
+    const produtosRef = admin.database().ref("produto");
+
+    produtosRef.once('value')
+        .then((snapshot) => {
+            const produtos = [];
+
+            snapshot.forEach((childSnapshot) => {
+                const produto = childSnapshot.val();
+                produto.id = childSnapshot.key;
+                produtos.push(produto);
+            });
+
+            res.json(produtos); // Enviar a resposta como JSON
+            console.log(produtos);
+        })
+        .catch((erro) => {
+            res.status(500).json({ error: erro.message }); // Enviar erro como JSON
+        });
+});
 
 
 app.post("/produto", (req, res) => {
-
     const produto = req.body
 
     admin.database().ref("produto").push(produto)
         .then(() => {
-            res.status(200).send('Pessoa criada com sucesso')
+            res.status(200).send('Produto criada com sucesso')
         })
         .catch((err) => {
             res.status(500).send(erro);
         })
 });
+//Atualizar um novvo produto
+//-Ne4aV92sRNMj0isCFrA
+
+app.post("/atualizar/:id", (req, res) => {
+    const produto = req.body
+    const id = req.params.id
+
+    admin.database().ref(`produto/${id}`).update(produto)
+        .then(() => {
+            res.send("O produto foi atualizado com sucesso")
+        })
+        .catch((erro) => {
+            res.status(500).send(erro)
+        })
+
+})
+
+//deletar um novo produto
+
+
+app.delete("/produto/:id", (req, res) => {
+    const id = req.params.id
+
+    admin.database().ref(`produto/${id}`).remove()
+        .then(() => {
+            res.send("O produto foi removido com sucesso")
+        })
+        .catch((error) => {
+            res.status(500).send(erro);
+        })
+
+})
 
 
 
